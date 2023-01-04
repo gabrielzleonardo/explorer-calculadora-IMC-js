9b6e2f60867b5c7f696129a2ed0db0eeff052984
Gabriel Leonardo <gleonardoalano@gmail.com>
2023-01-04 16:21:07 -0300

    feat: classificação do IMC

--- components/utilities.js
+++ components/utilities.js
@@ -1,12 +1,33 @@
 import Modal from "./Modal.js";
 
+let message;
+
 export const calculateIMC = (weight, height) =>
   (weight / (height * 0.01) ** 2).toFixed(2);
 
 export const notANumber = (value) => isNaN(value) || value == "";
 
 export const resultMessage = (result) => {
-  const message = `Seu IMC é de ${result}`;
+  if (result < 18.5) {
+    message = `Seu IMC é de ${result}
+      Classificação: MAGREZA`;
+  } else if (18.5 <= result <= 24.9)
+  {
+    message = `Seu IMC é de ${result}
+      Classificação: NORMAL`;
+  } else if (25 <= result <= 29.9)
+  {
+    message = `Seu IMC é de ${result}
+      Classificação: SOBREPESO`;
+  } else if (30 <= result <= 39.9)
+  {
+    message = `Seu IMC é de ${result}
+      Classificação: OBESIDADE`;
+  } else if (result > 40)
+  {
+    message = `Seu IMC é de ${result}
+      Classificação: OBESIDADE GRAVE`;
+  }
 
   Modal.message.innerText = message;
   Modal.open();
