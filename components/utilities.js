304a4d7990e3020b818ef8a29b187bd81e1a294e
Gabriel Leonardo <gleonardoalano@gmail.com>
2023-01-10 13:39:46 -0300

    mudança nos nomes de variaveis
    aplicação do cleancode

--- components/utilities.js
+++ components/utilities.js
@@ -1,6 +1,6 @@
 import Modal from "./Modal.js";
 
-let message;
+let messageDisplay;
 let type;
 let bgcolor;
 
@@ -12,32 +12,32 @@ export const notANumber = (value) => isNaN(value) || value == "";
 export const resultMessage = (result) => {
   if (result <  18.5) {
     bgcolor = `#ffcb00`;
-    message = `Seu IMC é de ${result}`;
+    messageDisplay = `Seu IMC é de ${result}`;
     type = "Classificação: MAGREZA";
     Modal.type.style.backgroundColor = bgcolor;
   } else if (result < 25) {
     bgcolor = `#5abae0`;
-    message = `Seu IMC é de ${result}`;
+    messageDisplay = `Seu IMC é de ${result}`;
     type = "Classificação: NORMAL";
     Modal.type.style.backgroundColor = bgcolor;
   } else if (result < 30) {
     bgcolor = `#c85383`;
-    message = `Seu IMC é de ${result}`;
+    messageDisplay = `Seu IMC é de ${result}`;
     type = "Classificação: SOBREPESO";
     Modal.type.style.backgroundColor = bgcolor;
   } else if (result < 40) {
     bgcolor = `#e11a21`;
-    message = `Seu IMC é de ${result}`;
+    messageDisplay = `Seu IMC é de ${result}`;
     type = "Classificação: OBESIDADE";
     Modal.type.style.backgroundColor = bgcolor;
   } else  {
     bgcolor = `#c3272f`;
-    message = `Seu IMC é de ${result}`;
+    messageDisplay = `Seu IMC é de ${result}`;
     type = "Classificação: OBESIDADE GRAVE";
     Modal.type.style.backgroundColor = bgcolor;
   }
 
-  Modal.message.innerText = message;
+  Modal.message.innerText = messageDisplay;
   Modal.type.innerText = type;
   Modal.open();
 };
